f02fb082ae24cd03e53545eda703a56b91029a94
bianqipeng <bianqipeng@bluemoon.com.cn>
2021-02-07 16:11:54 +0800

    redis测试案例

---
+++ spring-boot-07-redis/src/main/java/com/example/SpringBoot07RedisApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot07RedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot07RedisApplication.class, args);
+    }
+
+}
